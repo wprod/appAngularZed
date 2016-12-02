1e9df7b5a4920745a34a0c891207f76b1c7b6624
wprod <witters.amand@gmail.com>
2016-12-02 14:53:15 +0100

    google map

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-var app = angular.module('MonApp', ['ngRoute']);
+var app = angular.module('MonApp', ['ngRoute', 'ngAnimate', 'ngMap']);
 
-    app.config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'partials/home.html',
-                controller: 'PostsCtrl'
-            })
-            .when('/comments/:id', {
-                templateUrl: 'partials/comments.html',
-                controller: 'CommentsCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    })
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'PostsCtrl',
+        })
+        .when('/comments/:id', {
+            templateUrl: 'partials/comments.html',
+            controller: 'CommentsCtrl',
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+})
--- js/controller/comments.js
+++ js/controller/comments.js
@@ -1,28 +1,52 @@
-app.controller('CommentsCtrl', function ($scope, PostFactory, $routeParams, $rootScope) {
+app.controller('CommentsCtrl', function($scope, PostFactory, NgMap, $routeParams, $rootScope) {
 
     $rootScope.loading = true;
 
+
+    NgMap.getMap().then(function(map) {
+        console.log(map.getCenter());
+        console.log('markers', map.markers);
+        console.log('shapes', map.shapes);
+
+    });
+
+    $scope.reRenderMap = function() {
+        $timeout(function() {
+            angular.forEach($scope.maps, function(index) {
+                google.maps.event.trigger(index, 'resize');
+            });
+        }, 500);
+    }
+
+    $scope.maps = [];
+
+    $scope.$on('mapInitialized', function(evt, evtMap) {
+        $scope.maps.push(evtMap);
+    });
+
+
     $scope.newComment = {};
 
-    PostFactory.get($routeParams.id).then(function (post) {
+    PostFactory.get($routeParams.id).then(function(post) {
         $rootScope.loading = false;
         $scope.title = post.name;
         $scope.picture = post.picture;
         $scope.content = post.content;
         $scope.comments = post.comments;
         $scope.fullcontent = post.fullcontent;
-    }, function (msg) {
+        $scope.positions = post.positions;
+        console.log($scope.positions);
+    }, function(msg) {
         alert(msg);
     });
 
-
-    $scope.addComment = function () {
+    $scope.addComment = function() {
         $scope.comments.push($scope.newComment);
-                $scope.newComment = {};
+        $scope.newComment = {};
 
-        PostFactory.add($scope.newComment).then(function () {
+        PostFactory.add($scope.newComment).then(function() {
 
-        }, function () {
+        }, function() {
             alert('Erreur message non sauvegarde');
         });
 
